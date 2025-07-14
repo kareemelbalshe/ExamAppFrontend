a9f13ab1945058f07c9fc7e6f974aa5599c39004
Ahmed Essam <ahmed.essam.aladl@gmail.com>
2025-07-14 22:11:10 +0300

    ComfirmComponent Update

--- src/app/shared/confirm/confirm.service.ts
+++ src/app/shared/confirm/confirm.service.ts
@@ -5,9 +5,23 @@ export class ConfirmService {
   visible = signal(false);
   title = '';
   message = '';
+  isPrompt:boolean = false;
+  oKText:string = 'Confirm';
+  cancelText:string = 'Cancel';
+  isSuccess:boolean = true;
   callback: () => void = () => {};
 
-  show(title: string, message: string, onConfirm: () => void) {
+  show(title: string, message: string, onConfirm: () => void,{
+                                                                okText = 'Confirm', 
+                                                                isSuccess = true,
+                                                                isPrompt=  false,
+                                                                cancelText = 'Cancel'
+                                                    } :{ okText?: string, isSuccess?: boolean, isPrompt?: boolean, cancelText?: string } = {}
+  ) {
+    this.isPrompt = isPrompt?? false;
+    this.oKText = okText ?? 'Confirm';
+    this.cancelText = cancelText ?? 'Cancel';
+    this.isSuccess = isSuccess ?? true;
     this.title = title;
     this.message = message;
     this.callback = onConfirm;
